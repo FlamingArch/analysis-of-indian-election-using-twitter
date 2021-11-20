ca1c75fa6a0cecc8c7278380b163e0af561ddaa9
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-11-20 12:37:05 +0530

    Fixed Framer Motion Error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { motion } from "framer-motion";
+import { HandIcon } from "@heroicons/react/outline";
 
 function App() {
-  return <div>
-    Hello, World!
-  </div>;
+  return (
+    <motion.div
+      animate={{ scale: [1.2, 1], opacity: [0, 1] }}
+      transition={{ duration: 0.4 }}
+      className="grid h-screen w-screen place-content-center text-5xl font-light"
+    >
+      <div className="flex justify-center">
+        <HandIcon className="w-12 h-12 rotate-45" />
+        Hello, World!
+      </div>
+    </motion.div>
+  );
 }
 
 export default App;
