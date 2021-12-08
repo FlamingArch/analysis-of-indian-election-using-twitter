c108f8778d33a56bb690553a86516c528688bee2
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-12-09 00:03:56 +0530

    Update Button.js

--- src/Components/Supporting/Button.js
+++ src/Components/Supporting/Button.js
@@ -1,12 +1,26 @@
 import React from "react";
 
-export const Button = (props) => {
+const Button = (props) => {
+  function getButtonType(type) {
+    var raisedButton =
+      "bg-white text-black w-min px-4 py-2 rounded-md shadow-md hover:shadow-xl hover:scale-110 transform transition-all";
+    var iconButton =
+      "hover:bg-blue-900 hover:bg-opacity-20 w-min rounded-lg p-2 transition-all";
+    switch (type) {
+      case "raised":
+        return raisedButton;
+      case "icon":
+        return iconButton;
+      default:
+        return raisedButton;
+    }
+  }
+
   return (
-    <button
-      className="bg-white text-black w-min px-4 py-2 rounded-md shadow-md hover:shadow-xl hover:scale-110 transform transition-all"
-      onClick={props.onClick}
-    >
+    <button className={getButtonType(props.type)} onClick={props.onClick}>
       {props.children}
     </button>
   );
 };
+
+export default Button;
